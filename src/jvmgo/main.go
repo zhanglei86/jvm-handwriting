b2cf25a18a6102a85266e83d0447674d8bd539fe
zhanglei <zhanglei863@crnet.com.hk>
2019-03-31 00:06:46 +0800

    #9.ch03

--- src/jvmgo/main.go
+++ src/jvmgo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"jvmgo/classfile"
 	"jvmgo/classpath"
 	"strings"
 )
@@ -18,7 +19,7 @@ func main() {
 	}
 }
 
-func startJVM(cmd *Cmd) {
+func startJVM02(cmd *Cmd) {
 	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
 	fmt.Printf("classpath:%v class:%v args:%v\n", cp, cmd.class, cmd.args)
 
@@ -30,4 +31,43 @@ func startJVM(cmd *Cmd) {
 	}
 
 	fmt.Printf("class data:%v\n", classData)
+}
+
+func startJVM(cmd *Cmd) {
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	cf := loadClass(className, cp)
+	fmt.Println(cmd.class)
+	printClassInfo(cf)
+}
+
+func loadClass(className string, cp *classpath.Classpath) *classfile.ClassFile {
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		panic(err)
+	}
+
+	cf, err := classfile.Parse(classData)
+	if err != nil {
+		panic(err)
+	}
+
+	return cf
+}
+
+func printClassInfo(cf *classfile.ClassFile) {
+	fmt.Printf("version: %v.%v\n", cf.MajorVersion(), cf.MinorVersion())
+	fmt.Printf("constants count: %v\n", len(cf.ConstantPool()))
+	fmt.Printf("access flags: 0x%x\n", cf.AccessFlags())
+	fmt.Printf("this class: %v\n", cf.ClassName())
+	fmt.Printf("super class: %v\n", cf.SuperClassName())
+	fmt.Printf("interfaces: %v\n", cf.InterfaceNames())
+	fmt.Printf("fields count: %v\n", len(cf.Fields()))
+	for _, f := range cf.Fields() {
+		fmt.Printf("  %s\n", f.Name())
+	}
+	fmt.Printf("methods count: %v\n", len(cf.Methods()))
+	for _, m := range cf.Methods() {
+		fmt.Printf("  %s\n", m.Name())
+	}
 }
